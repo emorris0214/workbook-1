2a0f3f1dbebdc0ae37897e6bd6a4f99189c9b84d
unknown <Enoch Morris>
2025-04-09 11:58:42 -0400

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping List: 5 Oranges, 4 Apples, 3 Bananas, 2 Plums, 1 Avocado, Toilet Paper, Tissues, Soap, Ice, and Deodorant.");
+// your code goes here
+    }
+}
